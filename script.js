1d483fd658a5343a183cf9bf15558ba2383dd7bc
BloomVault <bloomvaultfarms@gmail.com>
2026-05-28 13:46:50 -0400

    Add success animation behavior

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const form = document.querySelector('.application');
+const successMessage = document.getElementById('successMessage');
 
 form.addEventListener('submit', () => {
   const button = form.querySelector('button');
+
   button.textContent = 'Submitting...';
   button.disabled = true;
+
+  setTimeout(() => {
+    successMessage.style.display = 'block';
+  }, 700);
 });
